e132f880272f3965c6184c7fbd1cb9714dea403f
zuzundrafx <workcel@yandex.ru>
2026-05-10 23:48:42 +0300

    Ranking Tiers patch

--- src/components/Pvp.tsx
+++ src/components/Pvp.tsx
@@ -448,8 +448,8 @@ const [showBetAmountIncrease, setShowBetAmountIncrease] = useState(false);
                             height: '100%',
                             objectFit: 'contain',
                             filter: isSelected 
-      ? 'saturate(0.7) hue-rotate(-35deg) brightness(0.8) drop-shadow(0 0 10px rgba(255, 255, 255, 0.6)) drop-shadow(0 0 20px rgba(255, 255, 255, 0.4))' 
-      : `saturate(0.7) hue-rotate(-35deg) brightness(0.8)`,
+      ? 'saturate(0.65) hue-rotate(-20deg) brightness(0.8) drop-shadow(0 0 10px rgba(255, 255, 255, 0.6)) drop-shadow(0 0 20px rgba(255, 255, 255, 0.4))' 
+      : `saturate(0.65) hue-rotate(-20deg) brightness(0.8)`,
                             opacity: isSelected ? 1 : (unlocked ? 1 : 0.4),
                             position: 'relative',
                             zIndex: 1,
@@ -507,7 +507,7 @@ const [showBetAmountIncrease, setShowBetAmountIncrease] = useState(false);
                               width: '100%',
                               height: '100%',
                               objectFit: 'contain',
-                              filter: `saturate(0.7) hue-rotate(-35deg) brightness(0.8)`,
+                              filter: `saturate(0.65) hue-rotate(-20deg) brightness(0.8)`,
                               position: 'relative',
                               opacity: isSelected ? 1 : (unlocked ? 1 : 0.4),
                             }}
