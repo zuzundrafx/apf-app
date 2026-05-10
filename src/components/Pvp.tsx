d7a0e90612964eb4aa806934736f80b51e1a0cdb
zuzundrafx <workcel@yandex.ru>
2026-05-10 23:46:29 +0300

    Ranking Tiers patch

--- src/components/Pvp.tsx
+++ src/components/Pvp.tsx
@@ -468,7 +468,7 @@ const [showBetAmountIncrease, setShowBetAmountIncrease] = useState(false);
                           alignItems: 'center',
                           justifyContent: 'center',
                           color: '#FFFFFF',
-                          fontSize: 'clamp(10px, 3vw, 16px)',
+                          fontSize: 'clamp(12px, 5vw, 20px)',
                           fontWeight: 700,
                           opacity: isSelected ? 1 : (unlocked ? 1 : 0.4),
                           zIndex: 2,
