6501e859f6ea752bbb387ea4ad12bfd73c66bb07
zuzundrafx <workcel@yandex.ru>
2026-05-17 15:20:28 +0300

    Client patch

--- src/App.tsx
+++ src/App.tsx
@@ -725,7 +725,7 @@ function App() {
              
                           <div className="tournament-card">
                             <div className="tournament-stats-text">
-                              <span>Base:</span>{totalDamage}/<span>PvP:</span>{pvpDamage}
+                              Base:{totalDamage} / PvP:{pvpDamage}
                             </div>
                             <div className="tournament-card-image">
                               <img src={`${BASE_URL}/UFC_cardpack.png`} alt="pack" />
